bd588953cedd4e1363dede986dd1b8a1839d9195
Spencer Fox <spencer.fox85@gmail.com>
2017-09-25 15:55:52 -0500

    update alpha runs for multiple import types

--- R/create_alpha_like_job_file.R
+++ R/create_alpha_like_job_file.R
@@ -9,14 +9,15 @@ if(grepl('wrangler', Sys.info()['nodename'])) setwd(file.path('/home/02958/sjf82
 # reporting_rate <- c(0.01, 0.0282, 0.0574, 0.0866, 0.1, 0.2)
 include_trans <- c("NA", "1", "5")
 temperature <- c("historic", "actual")
+extra_imports <- c(0,1)
 
 sink('launcher/alpha_mcmc_runs.txt')
-# for(ind in seq_along(reporting_rate)){
+for(ind in seq_along(extra_imports)){
   for(trans_ind in seq_along(include_trans)){
     for(temp_ind in seq_along(temperature)){
       startCmd <- "R CMD BATCH --no-restore --no-save '--args"
       #fileCmd <- paste0(' single_rnot=', single_rnot, ' rnot_value="', rnot_values[ind], '"')
-      fileCmd <- paste0(' temperature="', temperature[temp_ind], '" include_trans="', include_trans[trans_ind], '"')
+      fileCmd <- paste0(' temperature="', temperature[temp_ind], '" include_trans="', include_trans[trans_ind], '" extra_imports="', extra_imports[ind], '"')
       endCmd <- "' ../R/calc_alpha_posterior.R"
       full_cmd <- paste0(startCmd, fileCmd, endCmd)
       # print(full_cmd)
@@ -24,5 +25,5 @@ sink('launcher/alpha_mcmc_runs.txt')
       cat('\n')              # add new line
     }
   }
-# }
+}
 sink()
