9f9ce17050a1bd1c6e19c70cb08581430011cecb
Spencer Fox <spencer.fox85@gmail.com>
2017-10-07 13:55:36 -0500

    fitting now only goes through 2016

--- R/fitting_fxns.R
+++ R/fitting_fxns.R
@@ -57,12 +57,13 @@ get_alpha_parms_r0_dist_mcmc <- function(tx_data, curr_date, county_r0_dists){
 get_mcmc_parm_list <- function(include_trans,  temperature, last_only=FALSE, extra_imports = FALSE){
   ## Function gives the full parm list for running mcmc on every single date of importation
   ## Can call this function with specified parms, and get a list with elements ready-to-go for mcmc
-  tx_imports <- read_csv("data/Zika Disease Cases by Notification Date as of 030617.csv")
+  tx_imports <- read_csv("data/Zika Disease Cases as of 09282017.csv")
   tx_imports <- tx_imports %>% mutate(notification_date = mdy(`First Notification Date`)) %>%
     arrange(notification_date)%>%
     mutate(month = as.character(month(notification_date, label=TRUE, abbr = T)),
            county = tolower(str_replace_all(County, " County", ""))) %>%
-    select(county, notification_date, month)
+    select(county, notification_date, month) %>%
+    filter(year(notification_date) == 2016)
 
   if(extra_imports){
     ## If extra imports scenario, assume you've only seen 0.0574 % of imports, so replicate each row 17 times
